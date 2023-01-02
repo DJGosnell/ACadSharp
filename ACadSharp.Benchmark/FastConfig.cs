c89004314c0a7328ad42749372483ab675229e1b
DJGosnell <DJGosnell@gmail.com>
2023-01-01 23:31:57 -0500

    Added benchmark

---
+++ ACadSharp.Benchmark/FastConfig.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+
+namespace ACadSharp.Benchmark;
+public class FastConfig : ManualConfig
+{
+    public FastConfig()
+    {
+        Add(DefaultConfig.Instance);
+        AddJob(Job.Default
+            .WithLaunchCount(1)
+            .WithWarmupCount(1)
+            .WithIterationCount(1)
+        );
+    }
+}
